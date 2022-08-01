bfeff2e07d059db9260482a0b21b4e745c86ab16
urd00m <alan.linghao.wang@gmail.com>
2022-08-01 16:57:14 -0500

    got cpuid information after some research into it and verified most of the cache information found only except for L1 data cache which is slighty larger

--- cache_timer/cpuid_reader.c
+++ cache_timer/cpuid_reader.c
@@ -13,13 +13,54 @@
 #endif
 
 /*
-  Analysis on cpuid eax=2 outputs
+  Analysis on cpuid outputs
 */
+
+
+
+// ret is a 4 uint64_t array
+// a -> eax
+// b -> ebx
+// c -> ecx
+// d -> edx
+void cpuid(int leaf, int* ret) {
+  // run cpuid 
+  uint64_t a = leaf, b, c, d;
+  asm ( "mov $3, %%ecx\n"
+        "cpuid\n\t"
+        : "=a" (a), "=b" (b), "=c" (c), "=d" (d)
+        : "0" (a)
+        );
+  ret[0] = a;
+  ret[1] = b;
+  ret[2] = c;
+  ret[3] = d; 
+}
+
 int main(void) {
   INFO("Starting\n"); // starting test
+  int cpuid_ret[4];
+  enum cpuid_reg {eax, ebx, ecx, edx}; 
 
-  asm("mov $4, %eax"); // prime eax=4 for tlb and cache information
-  asm("mov $1, %ecx"); // prime ecx=0 for leaf 4
-  asm("cpuid"); // call cpuid
-  return 0;
+  cpuid(4, cpuid_ret);
+
+  INFO("eax: %x\n", cpuid_ret[eax]);
+  INFO("ebx: %x\n", cpuid_ret[ebx]);
+  INFO("ecx: %x\n", cpuid_ret[ecx]);
+  INFO("edx: %x\n", cpuid_ret[edx]);
+
+  // bits 04-00 of eax
+  uint64_t cache_level = 0;
+  cache_level = (cpuid_ret[eax] & 0xf); // 1111
+  printf("\n");
+  INFO("Cache level: %d\n", cache_level);
+
+  // cache information
+  int ebx_ret = cpuid_ret[ebx];
+  int ecx_ret = cpuid_ret[ecx];
+  INFO("ways: %d\n", ((ebx_ret & 0xffc00000)>>22)+1); // 31:22, 0xffc0 0000
+  INFO("partitions: %d\n", ((ebx_ret & 0x003ff000)>>12)+1); // 21:12, 0x003f f000
+  INFO("line_size: %d\n", (ebx_ret & 0x00000fff)+1); // 11:0, 0x0000 0fff
+  INFO("sets: %d\n", ecx_ret+1);
+  return 0; 
 }
--- cache_timer/i7_writeup.md
+++ cache_timer/i7_writeup.md
@@ -4,7 +4,7 @@ This is a 10th generation Intel I7 chip.
 ## General Information 
 Information according to Intel's datasheet for 10th generation processors. And CPUID informatino about cache and tlb information. Documentation here: https://c9x.me/x86/html/file_module_x86_id_45.html and here https://www.felixcloutier.com/x86/cpuid#tbl-3-12. 
 
-L1 cache: 42KB data cache, 32KB instruction cache, 64 bit cache line, 8-way  
+L1 cache: 42KB (48KB reading CPUID) data cache, 32KB instruction cache, 64 byte cache line, 8-way  
 L2 cache: 512KB total, 8-way  
 L3 cache: 8 MB shared (2 MB per core times 4 cores, 16-way)  
 
@@ -18,3 +18,32 @@ f2 = use cpuid leaf 18h to query tlb parameters
 ff = cpuid doesn't support information use leaf 4h to query cache parameters  
 
 #### CPUID return information (eax=4, leaf 4): 
+ecx=0, the data cache (L1d):  
+ways: 12  
+partitions: 1  
+line size: 64 bytes  
+sets: 64  
+total size: 48KB  
+
+ecx=1, the instruction cache (l1i):  
+ways: 8  
+partitions: 1  
+line size: 64 bytes  
+sets: 64  
+total size: 32KB  
+
+ecx=2, the L2 cache (unified cache):  
+ways: 8  
+partitions: 1  
+line size: 64  
+sets: 1024  
+total size: 512KB  
+
+ecx=3, the L3 cache (unified cache):  
+ways: 16  
+partitions: 1  
+line size: 64  
+sets: 8192  
+total size: 8MB  
+
+#### CPUID return information (eax=0x18, leaf 18) 
