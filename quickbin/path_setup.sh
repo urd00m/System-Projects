8fe45ca51f5a5ed801a13c7224bad81095a12abe
urd00m <alan.wang@anl.gov>
2023-01-19 21:26:44 -0600

    path issues

--- quickbin/path_setup.sh
+++ quickbin/path_setup.sh
@@ -6,3 +6,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # setup path
 export PATH=$PATH:${SCRIPT_DIR}/bin
 export QBIN_DIR=${SCRIPT_DIR}/bin
+export PROJECT_DIR=${SCRIPT_DIR}/..
